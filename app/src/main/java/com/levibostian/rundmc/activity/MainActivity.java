f1cb3cdba38f83046e047b146b3d74933dcc81b8
Levi Bostian <levi.bostian@gmail.com>
2015-04-11 16:47:13 -0500

    Flashing between group inverted and normal images working.

--- app/src/main/java/com/levibostian/rundmc/activity/MainActivity.java
+++ app/src/main/java/com/levibostian/rundmc/activity/MainActivity.java
@@ -1,20 +1,60 @@
 package com.levibostian.rundmc.activity;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import com.levibostian.rundmc.R;
+import com.levibostian.rundmc.fragment.MainFragment;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int RUN_DMC_GROUP_TOGGLE_INTERVAL = 100;
+
+    private Handler mHandler;
+    private Runnable mUpdater;
+    private HandlerListener mListener;
+
+    public interface HandlerListener {
+        void postUpdate();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mHandler = new Handler();
         setContentView(R.layout.activity_main);
+
+        mUpdater = new Runnable() {
+            @Override
+            public void run() {
+                mListener.postUpdate();
+                mHandler.postDelayed(mUpdater, RUN_DMC_GROUP_TOGGLE_INTERVAL);
+            }
+        };
+
+        setupFragment();
     }
 
+    private void setupFragment() {
+        MainFragment fragment = MainFragment.newInstance();
+        mListener = fragment;
+
+        fragment.setStartHandlerListener(new MainFragment.StartHandlerListener() {
+            @Override
+            public void startHandler() {
+                mHandler.post(mUpdater);
+            }
+        });
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/levibostian/rundmc/fragment/MainFragment.java
+++ app/src/main/java/com/levibostian/rundmc/fragment/MainFragment.java
@@ -1,21 +1,47 @@
 package com.levibostian.rundmc.fragment;
 
+import android.app.Fragment;
 import android.media.MediaPlayer;
-import android.support.v4.app.Fragment;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import com.levibostian.rundmc.R;
+import com.levibostian.rundmc.activity.MainActivity;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements MainActivity.HandlerListener {
 
     private ImageView mRunDmcButton;
     private ImageView mRunDmcGroupImageView;
+    private ImageView mRunDmcGroupInvertImageView;
+
+    private int mCurrentRunDmcImageRes = R.drawable.run_dmc;
 
     private MediaPlayer mMediaPlayer;
+
+    private StartHandlerListener mListener;
+
+    @Override
+    public void postUpdate() {
+        toggleRunDmcGroupImage();
+    }
+
+    public interface StartHandlerListener {
+        void startHandler();
+    }
+
+    public void setStartHandlerListener(StartHandlerListener listener) {
+        mListener = listener;
+    }
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,10 +56,16 @@ public class MainFragment extends Fragment {
 
         mRunDmcButton = (ImageView) view.findViewById(R.id.run_dmc_button);
         mRunDmcGroupImageView = (ImageView) view.findViewById(R.id.run_dmc_group);
-
-        setupViews();
+        mRunDmcGroupInvertImageView = (ImageView) view.findViewById(R.id.run_dmc_invert_group);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setupViews();
     }
 
     private void setupViews() {
@@ -43,12 +75,78 @@ public class MainFragment extends Fragment {
                 playMyAdidas();
 
                 showImages();
+
+                mRunDmcButton.setVisibility(View.GONE);
             }
         });
     }
 
     private void showImages() {
         mRunDmcGroupImageView.setVisibility(View.VISIBLE);
+        // mRunDmcGroupImageView.setImageResource(R.drawable.my);
+
+//        Thread thread = new Thread() {
+//            @Override
+//            public void run() {
+//                try {
+//                    while (!isInterrupted()) {
+//                        Thread.sleep(RUN_DMC_GROUP_TOGGLE_INTERVAL);
+//                        getActivity().runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                toggleRunDmcGroupImage();
+//                            }
+//                        });
+//                    }
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//
+//        thread.start();
+
+        mListener.startHandler();
+
+//        new CountDownTimer(1000, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                Log.d("whatever", "tick");
+//                toggleRunDmcGroupImage();
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//
+//            }
+//        }.start();
+
+//        getActivity().runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                toggleRunDmcGroupImage();
+//            }
+//        });
+    }
+
+    private void toggleRunDmcGroupImage() {
+        Log.d("whatever", "toggled");
+
+//        if (mRunDmcGroupImageView.getVisibility() == View.VISIBLE) {
+//            mRunDmcGroupImageView.setVisibility(View.GONE);
+//            mRunDmcGroupInvertImageView.setVisibility(View.VISIBLE);
+//        } else {
+//            mRunDmcGroupImageView.setVisibility(View.VISIBLE);
+//            mRunDmcGroupInvertImageView.setVisibility(View.GONE);
+//        }
+
+        if (mCurrentRunDmcImageRes == R.drawable.run_dmc_invert) {
+            mCurrentRunDmcImageRes = R.drawable.run_dmc;
+        } else {
+            mCurrentRunDmcImageRes = R.drawable.run_dmc_invert;
+        }
+
+        mRunDmcGroupImageView.setImageResource(mCurrentRunDmcImageRes);
     }
 
     private void playMyAdidas() {
