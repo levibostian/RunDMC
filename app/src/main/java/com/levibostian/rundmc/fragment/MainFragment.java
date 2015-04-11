f02368b6ea6a9402696c99d722883516fd2a1876
Levi Bostian <levi.bostian@gmail.com>
2015-04-11 13:15:57 -0500

    Initial commit. Create basic app via studio.

---
+++ app/src/main/java/com/levibostian/rundmc/fragment/MainFragment.java
@@ -0,0 +1,19 @@
+package com.levibostian.rundmc.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.levibostian.rundmc.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
