fc8af2ac65a489db34caea270d839692d99c689a
Levi Bostian <levi.bostian@gmail.com>
2015-04-11 18:19:30 -0500

    Move all MainFragment data into MainActivity to save time.

--- app/src/main/java/com/levibostian/rundmc/activity/MainActivity.java
+++ app/src/main/java/com/levibostian/rundmc/activity/MainActivity.java
@@ -1,66 +1,106 @@
 package com.levibostian.rundmc.activity;
 
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import com.levibostian.rundmc.R;
-import com.levibostian.rundmc.fragment.MainFragment;
-
 
 public class MainActivity extends ActionBarActivity {
 
     private static final int RUN_DMC_GROUP_TOGGLE_INTERVAL = 100;
 
-    private Handler mHandler;
-    private Runnable mUpdater;
-    private HandlerListener mListener;
+    private ImageView mRunDmcButton;
+    private ImageView mRunDmcGroupImageView;
 
-    public interface HandlerListener {
-        void postUpdate();
-    }
+    private int mCurrentRunDmcImageRes = R.drawable.run_dmc;
+
+    private MediaPlayer mMediaPlayer;
+
+    private Handler mHandler;
+    private Runnable mRunDmcGroupRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mHandler = new Handler();
         setContentView(R.layout.activity_main);
 
-        mUpdater = new Runnable() {
+        mRunDmcButton = (ImageView) findViewById(R.id.run_dmc_button);
+        mRunDmcGroupImageView = (ImageView) findViewById(R.id.run_dmc_group);
+
+        mHandler = new Handler();
+        mRunDmcGroupRunnable = new Runnable() {
             @Override
             public void run() {
-                mListener.postUpdate();
-                mHandler.postDelayed(mUpdater, RUN_DMC_GROUP_TOGGLE_INTERVAL);
+                toggleRunDmcGroupImage();
+                mHandler.postDelayed(mRunDmcGroupRunnable, RUN_DMC_GROUP_TOGGLE_INTERVAL);
             }
         };
 
-        setupFragment();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    private void playMyAdidas() {
+        mMediaPlayer = MediaPlayer.create(this, R.raw.my_adidas);
+        mMediaPlayer.start();
+    }
+
+    private void showImages() {
+        mRunDmcGroupImageView.setVisibility(View.VISIBLE);
+
+        mHandler.post(mRunDmcGroupRunnable);
+    }
+
+    private void toggleRunDmcGroupImage() {
+        Log.d("whatever", "toggled");
+
+        if (mCurrentRunDmcImageRes == R.drawable.run_dmc_invert) {
+            mCurrentRunDmcImageRes = R.drawable.run_dmc;
+        } else {
+            mCurrentRunDmcImageRes = R.drawable.run_dmc_invert;
+        }
+
+        mRunDmcGroupImageView.setImageResource(mCurrentRunDmcImageRes);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        mHandler.removeCallbacks(mUpdater);
+        mHandler.removeCallbacks(mRunDmcGroupRunnable);
+
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+
+            mMediaPlayer = null;
+        }
     }
 
-    private void setupFragment() {
-        MainFragment fragment = MainFragment.newInstance();
-        mListener = fragment;
+    @Override
+    public void onResume() {
+        super.onResume();
 
-        fragment.setStartHandlerListener(new MainFragment.StartHandlerListener() {
+        setupViews();
+    }
+
+    private void setupViews() {
+        mRunDmcButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void startHandler() {
-                mHandler.post(mUpdater);
+            public void onClick(View v) {
+                playMyAdidas();
+
+                showImages();
+
+                mRunDmcButton.setVisibility(View.GONE);
             }
         });
-
-        getFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit();
     }
 
     @Override
